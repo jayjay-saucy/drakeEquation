214206e443cdf2b1104dd7e3fe7abd411c3fd890
Jayllian Marquardt <jayllian.marquardt.2021@student.lhschools.org>
2020-02-13 14:05:29 -0500

    Created drake equation variables

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -19,6 +19,14 @@ fc = 0.1 to 0.2 (10–20% of which will be able to communicate)
 L = 1000 to 100,000,000 years (which will last somewhere between 1000 and 100,000,000 years)
  */
 
+    int N;
+    int rStar;
+    double f _p;
+    int numberOfExoplanets;
+    double f _l;
+    double f_i;
+    double f _c;
+    int L;
 
 
     }
